18e0e49fbd196ccb840000d2bd0d38bd723adaf9
Lev Geer <leon.geer@bbzbl-it.ch>
2025-08-23 19:07:56 +0200

    Add two little mistakes for TDD Documentation

--- src/main/java/com/business/services/ProductServices.java
+++ src/main/java/com/business/services/ProductServices.java
@@ -9,92 +9,83 @@ import org.springframework.stereotype.Component;
 
 import com.business.entities.Product;
 import com.business.repositories.ProductRepository;
+
 @Component
-public class ProductServices 
-{
+public class ProductServices {
 	@Autowired
 	private ProductRepository productRepository;
 
-	//add Product
-	public void addProduct(Product p)
-	{
+	// add Product
+	public void addProduct(Product p) {
 		this.productRepository.save(p);
 	}
 
-
-	//getAll products
-	public List<Product> getAllProducts()
-	{
-		List<Product> products=(List<Product>)this.productRepository.findAll();
+	// getAll products
+	public List<Product> getAllProducts() {
+		List<Product> products = (List<Product>) this.productRepository.findAll();
 		return products;
 	}
 
-	//get Single Product
-	public Product getProduct(int id)
-	{
+	// get Single Product
+	public Product getProduct(int id) {
 		Optional<Product> optional = this.productRepository.findById(id);
-		Product product=optional.get();
+		Product product = optional.get();
 		return product;
 	}
 
-	//update Product
-	public void updateproduct(Product p,int id)
-	{
+	// update Product
+	public void updateproduct(Product p, int id) {
 		p.setPid(id);
 		Optional<Product> optional = this.productRepository.findById(id);
-		Product prod=optional.get();
+		Product prod = optional.get();
 
-		if(prod.getPid()==id)
-		{
-			this.productRepository.save(p);				
+		if (prod.getPid() == id) {
+			this.productRepository.save(p);
 		}
 	}
-	//delete product
-	public void deleteProduct(int id)
-	{
+
+	// delete product
+	public void deleteProduct(int id) {
 		this.productRepository.deleteById(id);
 	}
 
-	//Get Product By Name
-	public Product getProductByName(String name)
-	{
-		
-		Product product= this.productRepository.findByPname(name);
-		if(product!=null)
-		{
+	// Get Product By Name
+	public Product getProductByName(String name) {
+
+		Product product = this.productRepository.findByPname(name);
+		if (product != null) {
 			return product;
 		}
 		return null;
-	
+
 	}
 
-    public double calculateFinalPrice(double basePrice, int id) {
+	public double calculateFinalPrice(double basePrice, int id) {
 
-        Product product = this.getProduct(id);
-        double price = basePrice;
+		Product product = this.getProduct(id);
+		double price = basePrice;
 
+		if (price < 100) {
+			price *= 1.07;
+		} else {
+			price *= 1.19;
+		}
 
-        if (price < 100) {
-            price *= 1.07;
-        } else {
-            price *= 1.19;
-        }
+		double weight = product.getPweight();
+		if (weight > 10) {
+			price += (weight - 10) * 2;
+		} else if (weight >= 5) { // mistake: includes weight = 5
+			price += (weight - 5) * 1.5;
+		} else {
+			price += weight * 1;
+		}
 
-        double weight = product.getPweight();
-        if (weight > 10) {
-            price += (weight - 10) * 2;
-        } else if (weight > 5) {
-            price += (weight - 5) * 1.5;
-        } else {
-            price += weight * 1;
-        }
+		if (price >= 500) { // subtle mistake: include 500
+			price += 50;
+		}
 
-        if (price > 500) {
-            price += 50;
-        }
-
-        product.setPprice(price);
-        this.productRepository.save(product);
-        return price;
-    }
+		product.setPprice(price);
+		this.productRepository.save(product);
+		return price;
+	}
 }
